872e49e176a641281fa3e612a9c4a4f3f34fd51e
Awais <awais.ahmad@ontariotechu.net>
2024-11-12 16:56:22 -0500

    Added DBHelper

---
+++ app/src/main/java/com/example/myapp/User.java
@@ -0,0 +1,75 @@
+package com.example.myapp;
+
+public class User {
+    private int id;
+    private long phone;
+    private String firstName, lastName, password, email, userType;
+
+    // create constructor
+    public User(int id, String firstName, String lastName, String password, long phone, String email, String userType) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+        this.userType = userType;
+        this.phone = phone;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
