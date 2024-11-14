b0f83ec724684f40c98100ba0dc0a29f2be7cbb9
Mithusan <mithusan.arul11@gmail.com>
2024-11-14 00:23:15 -0500

    Fixed Spo2 Characteristic Not being updated

--- app/src/main/java/com/example/healthtrack/PatientHealthData.java
+++ app/src/main/java/com/example/healthtrack/PatientHealthData.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -33,14 +34,17 @@ public class PatientHealthData extends AppCompatActivity {
     private BluetoothGatt bluetoothGatt;
 
     private static final int REQUEST_BLUETOOTH_PERMISSIONS = 1;
-
+    TextView heartRateView;
+    TextView spo2View;
     private int heartRateIndex = 0;
     private int oxygenIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overall_patient_health);  // Make sure this is correct
-
+        heartRateView = findViewById(R.id.heartRateTextView);
+        spo2View =findViewById(R.id.OxiTextView);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         device = bluetoothAdapter.getRemoteDevice(DEVICE_ADDRESS);
 
@@ -109,41 +113,62 @@ public class PatientHealthData extends AppCompatActivity {
 
                 // Get heart rate and oxygen characteristics
                 BluetoothGattCharacteristic heartRateCharacteristic = gatt.getService(SERVICE_UUID).getCharacteristic(HEARTRATE_CHAR_UUID);
-                BluetoothGattCharacteristic oxygenCharacteristic = gatt.getService(SERVICE_UUID).getCharacteristic(OXI_CHAR_UUID);
 
                 gatt.setCharacteristicNotification(heartRateCharacteristic, true);
-                gatt.setCharacteristicNotification(oxygenCharacteristic, true);
-                //gatt.readCharacteristic(heartRateCharacteristic);
-                //gatt.readCharacteristic(oxygenCharacteristic);
 
                 // Write the descriptor to enable notifications
                 BluetoothGattDescriptor heartRateDescriptor = heartRateCharacteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
-                BluetoothGattDescriptor oxygenDescriptor = oxygenCharacteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
-
                 if (heartRateDescriptor != null) {
                     heartRateDescriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                    Log.d("BluetoothGattCallback", "Heart Notif");
                     gatt.writeDescriptor(heartRateDescriptor); // Enable heart rate notifications
                 }
-
-                if (oxygenDescriptor != null) {
-                    oxygenDescriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
-                    gatt.writeDescriptor(oxygenDescriptor); // Enable SpO2 notifications
-                }
             }
         }
 
-        @Override
-        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+         @Override
+         public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
             if (HEARTRATE_CHAR_UUID.equals(characteristic.getUuid())) {
                 int heartRate = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-                Log.d("HeartRate", "Updated heart rate: " + heartRate);
 
-            } else if (OXI_CHAR_UUID.equals(characteristic.getUuid())) {
+                // Update the UI using runOnUiThread
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Update your UI elements here
+                        heartRateView.setText("Heart Rate: " + heartRate);
+                    }
+                });
+
+            } else if(OXI_CHAR_UUID.equals(characteristic.getUuid())){
                 int oxygenLevel = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-                Log.d("Oxygen", "Updated SpO2: " + oxygenLevel);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        spo2View.setText("O2: " + oxygenLevel);
+                    }
+                });
             }
         }
 
-    };
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d("DescriptorWrite", "Descriptor write successful for: " + descriptor.getUuid().toString());
+                BluetoothGattCharacteristic oxygenCharacteristic = gatt.getService(SERVICE_UUID).getCharacteristic(OXI_CHAR_UUID);
+                gatt.setCharacteristicNotification(oxygenCharacteristic, true);
 
+                BluetoothGattDescriptor oxygenDescriptor = oxygenCharacteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
+                if (oxygenDescriptor != null) {
+                    oxygenDescriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                    Log.d("BluetoothGattCallback", "O2 Notif");
+                    gatt.writeDescriptor(oxygenDescriptor); // Enable SpO2 notifications
+                }
+            } else {
+                Log.e("DescriptorWrite", "Descriptor write failed for: " + descriptor.getUuid().toString());
+            }
+        }
+    };
 }
