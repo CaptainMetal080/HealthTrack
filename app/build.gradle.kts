23094dbd7c0680e0fa475394e637010492c90e22
Mohammad Mohammadkhani <65586276+CaptainMetal080@users.noreply.github.com>
2024-11-14 21:28:36 -0500

    chart changed function position

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
-    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation (libs.mpandroidchart)
     implementation(libs.constraintlayout)
     implementation(libs.androidx.bluetooth)
     testImplementation(libs.junit)
